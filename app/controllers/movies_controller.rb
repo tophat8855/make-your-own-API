042bc32d60f40c7cb162d7855179dc7fb29e03e6
tophat8855 <hrfarley@gmail.com>
2015-01-26 15:28:15 -0800

    making homepage nice, search not working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,14 @@ class MoviesController < ActionController::Base
     @movies = Movie.all
     render json: @movies
   end
+
+  def search
+    @movies = Movie.find(params[])
+    render json: @movies
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :year, :plot, :image_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 root 'welcome#index'
 
 get '/all' => 'movies#all'
+get '/search' => 'movies#search'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
